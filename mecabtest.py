e9c17a661621a77e8d87b2559842fdebcc91bcfc
Yuki NOAKI <yuki@yukis-MBP-7.local>
2019-03-28 13:24:57 +0900

    fix surface bug

--- mecabtest.py
+++ mecabtest.py
@@ -2,14 +2,14 @@ import MeCab
 import sys
 
 def main():
-	if len(sys.argv)<2:
+	if len(sys.argv) < 2:
 		print("使い方: python3 mecabtest.py 解析したい文章")
 		return 1
 	mecab = MeCab.Tagger("-Ochasen")
-	mecab.parseToNode('')	
+	mecab.parseToNode('') #if not needed, remove.	
 	node = mecab.parseToNode(sys.argv[1])
 	while node:
-		word = node.surface
+		word = (node.surface).strip(node.next.surface)
 		hinshi = node.feature.split(",")[0] #1,2,..により詳しい品詞や活用が格納されている
 		print(word + " " + hinshi)
 		node = node.next

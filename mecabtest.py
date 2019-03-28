7312bfc404e8086e645ae9e0ac781204a9637ddf
Yuki NOAKI <yuki@yukis-MBP-7.local>
2019-03-28 11:19:27 +0900

    add MeCabtest

---
+++ mecabtest.py
@@ -0,0 +1,19 @@
+import MeCab
+import sys
+
+def main():
+	if len(sys.argv)<2:
+		print("使い方: python3 mecabtest.py 解析したい文章")
+		return 1
+	mecab = MeCab.Tagger("-Ochasen")
+	mecab.parseToNode('')	
+	node = mecab.parseToNode(sys.argv[1])
+	while node:
+		word = node.surface
+		hinshi = node.feature.split(",")[0] #1,2,..により詳しい品詞や活用が格納されている
+		print(word + " " + hinshi)
+		node = node.next
+	return 0
+
+if __name__ == '__main__':
+	main()

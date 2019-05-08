2993bb653b4cb87aebe53f86cf9490b3e9f16a97
en0qi <yknoaki@gmail.com>
2019-05-08 22:40:32 +0900

    SQL文法ミスの修正

--- insertToDB.go
+++ insertToDB.go
@@ -16,7 +16,7 @@ func UpdateTopic(topicList []structs.TopicStruct) (res int) {
 	for index := 0; index < len(topicList); index = index + 1 {
 		if topicList[index].IsNewTopic == false {
 
-			query := "SELECT newsID FROM topic WHERE id=" + strconv.FormatUint(topicList[index].ID, 10)
+			query := "SELECT newsid FROM topic WHERE id=" + strconv.FormatUint(topicList[index].ID, 10)
 			res, err := db.Exec(query)
 			if err != nil {
 				fmt.Println(err)
@@ -28,7 +28,7 @@ func UpdateTopic(topicList []structs.TopicStruct) (res int) {
 			newsIDarray = append(newsIDarray, topicList[index].AddedNewsID...) //追加するnewsIDをappend
 			//topicIDが存在しない→新たなtopicIDを振り、newsIDarrayを追加する topicIDが存在する→newsIDarrayを更新する
 			str := fmt.Sprintf("%v", newsIDarray)
-			query = "UPDATE topic SET newsID =" + str + " WHERE id = " + strconv.FormatUint(topicList[index].ID, 10)
+			query = "UPDATE topic SET newsid =\"" + str + "\" WHERE id = " + strconv.FormatUint(topicList[index].ID, 10)
 			res, err = db.Exec(query)
 			if err != nil {
 				fmt.Println(err)
@@ -39,7 +39,7 @@ func UpdateTopic(topicList []structs.TopicStruct) (res int) {
 		} else { //isNewTopic == true
 
 			str := fmt.Sprintf("%v", topicList[index].AddedNewsID)
-			query := "INSERT INTO topic (id, newsID) VALUES (" + strconv.FormatUint(topicList[index].ID, 10) + "," + str + ")"
+			query := "INSERT INTO topic (id, newsid) VALUES (" + strconv.FormatUint(topicList[index].ID, 10) + ",\"" + str + "\")"
 			res, err := db.Exec(query)
 			if err != nil {
 				fmt.Println(err)
@@ -57,7 +57,7 @@ func InsertNews(newsList []structs.NewsStruct) ([]structs.NewsStruct, error) {
 	defer db.Close()
 	var uint64index uint64 = 0
 	for index := 0; index < len(newsList); index = index + 1 {
-		query := "INSERT INTO news (title, url) VALUES (" + newsList[index].Title + "," + newsList[index].URL + ")"
+		query := "INSERT INTO news (title, url) VALUES (\"" + newsList[index].Title + "\",\"" + newsList[index].URL + "\")"
 		res, err := db.Exec(query)
 		if err != nil {
 			fmt.Println(err)

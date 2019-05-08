d78b59ea6d2f6a4b0508ddf2121147cc3bf393f8
Yuki NOAKI <yuki@yukis-MBP-7.local>
2019-05-08 17:36:21 +0900

    戻り値(0,-1)を追加, コメント追加

--- insertToDB.go
+++ insertToDB.go
@@ -6,26 +6,32 @@ import (
 	"~/connDB"
 )
 
-func UpdateTopic(st *makeTopic.TopicStruct) {
+func UpdateTopic(st *makeTopic.TopicStruct) res int {
 	db := connDB.Conndb()
 	defer db.Close()
 	//newsIDに関する部分は、maketopic.goの方と擦り合わせる
-	newsIDarray = append(newsIDarray, st.AddednewsID)
+	// newsIDarray := 追加前のnewsIDの配列
+	newsIDarray = append(newsIDarray, st.AddednewsID) //追加するnewsIDをappend
+	//topicIDが存在しない→新たなtopicIDを振り、newsIDarrayを追加する topicIDが存在する→newsIDarrayを更新する
 	query := "INSERT INTO topic (id, newsID) VALUES (" + st.ID + "," + newsIDarray + ") ON DUPLICATED KEY UPDATE newsID =" + st.AddednewsID
 	res, err := db.Exec(query)
 	if err != nil {
 		fmt.Println(err)
+		return -1
 	}
 	fmt.Println(res) //for debug
+	return 0
 }
 
-func InsertNews(st *makeTopic.NewsStruct) {
+func InsertNews(st *makeTopic.NewsStruct) res int {
 	db := connDB.Conndb()
 	defer db.Close()
 	query := "INSERT INTO news (title, url) VALUES (" + st.Title + "," + st.URL + ")"
 	res, err := db.Exec(query)
 	if err != nil {
 		fmt.Println(err)
+		return -1
 	}
 	fmt.Println(res) //for debug
+	return 0
 }

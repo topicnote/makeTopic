8de19bfee85faa3f05cc88d9b956f0274d263e6b
en0qi <yknoaki@gmail.com>
2019-05-08 21:33:00 +0900

    リストのポインタの消失

--- insertToDB.go
+++ insertToDB.go
@@ -7,12 +7,12 @@ import (
 	"../structs"
 )
 
-func UpdateTopic(topicList *[]structs.TopicStruct) (res int) {
+func UpdateTopic(topicList []structs.TopicStruct) (res int) {
 	db := connDB.Conndb()
 	defer db.Close()
 
 	// newsIDarray := 追加前のnewsIDの配列
-	for index := 0; index < len(&topicList); index = index + 1 {
+	for index := 0; index < len(topicList); index = index + 1 {
 		if topicList[index].isNewTopic == false {
 
 			query := "SELECT newsID FROM topic WHERE id=" + topicList[index].ID
@@ -27,8 +27,8 @@ func UpdateTopic(topicList *[]structs.TopicStruct) (res int) {
 			newsIDarray = append(newsIDarray, topicList[index].AddednewsID) //追加するnewsIDをappend
 			//topicIDが存在しない→新たなtopicIDを振り、newsIDarrayを追加する topicIDが存在する→newsIDarrayを更新する
 
-			query := "UPDATE topic SET newsID =" + newsIDarray + " WHERE id = " + topicList[index].ID
-			res, err := db.Exec(query)
+			query = "UPDATE topic SET newsID =" + newsIDarray + " WHERE id = " + topicList[index].ID
+			res, err = db.Exec(query)
 			if err != nil {
 				fmt.Println(err)
 				return -1
@@ -50,7 +50,7 @@ func UpdateTopic(topicList *[]structs.TopicStruct) (res int) {
 	return 0
 }
 
-func InsertNews(newsList *[]structs.NewsStruct) (*[]structs.NewsStruct, error) {
+func InsertNews(newsList []structs.NewsStruct) ([]structs.NewsStruct, error) {
 
 	db := connDB.Conndb()
 	defer db.Close()
